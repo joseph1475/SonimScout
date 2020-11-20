f590c485c65daa95c04ff49925cf032750a18421
minugms <minugms@gmail.com>
2020-11-20 10:28:14 +0530

    Minu 1st commit

--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -1,5 +1,7 @@
 import pytest
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 
 @pytest.mark.usefixtures("setup")
 class BaseClass:
@@ -14,6 +16,8 @@ class BaseClass:
     def SafeGuardDisabling(self):
         self.driver1.find_element_by_id("com.sonim.safeguard:id/switch_bar").click()
 
-
+    def verifyElementPresence(self,elementtext):
+        elementwait = WebDriverWait(self.driver, 5).until(
+            expected_conditions.presence_of_all_elements_located((By.XPATH, elementtext)))
 
 
