bf2e4ed880b1cceb1a115489d02593e67048bbc4
minu <minugms@gmail.com>
2020-12-17 17:00:14 +0530

    Minu_test5

--- tests/test_5RestrictFeatures.py
+++ tests/test_5RestrictFeatures.py
@@ -104,6 +104,9 @@ class TestRestrictFeatures(BaseClass):
         self.driver1.find_element_by_android_uiautomator("new UiSelector().textContains(\"Restore Factory Settings\")").click()
         #self.driver1.find_element_by_xpath("//android.widget.TextView[@text='Restore Factory Settings]").click()
         self.driver1.find_element_by_xpath("//*[@text='SAVE']").click()
+        #self.press_keycode(4)
+        self.driver1.find_element_by_android_uiautomator("new UiSelector().textContains(\"Application PIN Timeout\")").click()
+        self.driver1.find_element_by_xpath("//*[@text='30 Seconds']").click()
         desired_capsSettings = {
                 "automationName": "UiAutomator2",  # Appium (default), or UiAutomator2
                 "platformName": "Android",  # Andriod,IOS
@@ -118,9 +121,38 @@ class TestRestrictFeatures(BaseClass):
         time.sleep(5)
         Settingsdriver.find_element_by_xpath("//*[@text='System']").click()
         Settingsdriver.find_element_by_xpath("//*[@text='Advanced']").click()
-        Settingsdriver.find_element_by_xpath("//*[@text='Reset options']").click()
+        Settingsdriver.find_element_by_android_uiautomator(
+            "new UiSelector().textContains(\"Reset options\")").click()
         Settingsdriver.find_element_by_xpath("//*[@text='Erase all data (factory reset)']").click()
         enterpin =Settingsdriver.find_element_by_xpath("//*[@text='Enter PIN']").text
         assert enterpin == "Enter PIN"
         Settingsdriver.find_element_by_id("com.sonim.safeguard:id/editText").send_keys(1234)
         Settingsdriver.find_element_by_xpath("//*[@text='OK']").click()
+
+    def test_timeout(self):
+        time.sleep(5)
+        desired_capsSettings = {
+            "automationName": "UiAutomator2",  # Appium (default), or UiAutomator2
+            "platformName": "Android",  # Andriod,IOS
+            "platformVersion": "",  # can be open type or can declare the version .Eg: 8.1.1
+            "deviceName": "903890ef",  # Android Emulator or 903e900a device ID
+            "appPackage": "com.android.settings",
+            "appActivity": "com.android.settings.Settings"
+        }
+        Settingsdriver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capsSettings)
+        Settingsdriver.find_element_by_android_uiautomator(
+            "new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"" + "System" + "\").instance(0))");
+        time.sleep(5)
+        Settingsdriver.find_element_by_xpath("//*[@text='System']").click()
+        Settingsdriver.find_element_by_xpath("//*[@text='Advanced']").click()
+        Settingsdriver.find_element_by_android_uiautomator(
+            "new UiSelector().textContains(\"Reset options\")").click()
+        Settingsdriver.find_element_by_xpath("//*[@text='Erase all data (factory reset)']").click()
+        title = Settingsdriver.find_element_by_id("com.android.settings:id/suc_layout_title").text
+        assert title == "Erase all data (factory reset)"
+        Settingsdriver.press_keycode(4)
+        time.sleep(40)
+        Settingsdriver.find_element_by_xpath("//*[@text='Erase all data (factory reset)']").click()
+        enterpin = Settingsdriver.find_element_by_xpath("//*[@text='Enter PIN']").text
+        assert enterpin == "Enter PIN"
+
